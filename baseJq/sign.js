51019735975ab6f8cc28ff764e7b29f84afb8947
xueqiwei <xueqiwei@163.com>
2018-04-12 10:36:39 +0800

    修正排版

--- baseJq/sign.js
+++ baseJq/sign.js
@@ -17,99 +17,98 @@
  */
 ;
 (function ($) {
-    //构造函数，传入dom本身和参数
-    $.fn.sign = function (options) {
-        //需要暴露保存和清除的两个方法
-        if (typeof options == 'string') {
-            return $.fn.sign.methods[options](this);
-        }
-        var self = $(this)
-        //合并参数
-        options = $.extend({}, $.fn.sign.default, options || {})
-        //canvas具体设置
-        var canDom = init(self, options)[0]
-        var context = canDom.getContext('2d')//画布上下文
-        var canvasX = canDom.getBoundingClientRect().left; // 画板的坐标x
-        var canvasY = canDom.getBoundingClientRect().top; // 画板的坐标y
-        context.fillStyle = options.background; // 画布背景色
-        context.lineWidth = options.lineWidth; // 线的宽度
-        context.strokeStyle = options.lineColor; // 线的颜色
-        context.fillRect(0, 0, canDom.width, canDom.height); // 画板的范围        
-
-        //事件
-        canDom.addEventListener("mousedown", down, false);
-        canDom.addEventListener("mousemove", draw, false);
-        canDom.addEventListener("mouseup", up, false);
-
-        var onoff = false; // 锁定开关
-        var oldx = canvasX; // 起始坐标x
-        var oldy = canvasY; // 起始坐标y
-        var newx; // 结束坐标x
-        var newy; // 结束坐标y
-        // 鼠标按下
-        function down(event) {
-            onoff = true; // 打开开关
-            oldx = event.clientX - canvasX; // 鼠标在画板中点击的X的坐标
-            oldy = event.clientY - canvasY; // 鼠标在画板中点击的Y的坐标
-            context.beginPath(); // 开始路径
-        }
-
-        // 鼠标移动
-        function draw(event) {
-            // 开关
-            if (onoff) {
-                newx = event.clientX - canvasX;
-                newy = event.clientY - canvasY;
-                context.moveTo(oldx, oldy); // 线的起点坐标
-                context.lineTo(newx, newy); // 线的始点坐标
-                context.stroke(); // 初始化到画布中
-                oldx = newx;
-                oldy = newy;
-            }
-        }
-        // 鼠标离开
-        function up() {
-            onoff = false; // 关闭开关
-            context.closePath(); // 关闭路径
-        };
-        //初始化设置容器和画布，并且返回canvas对象
-        function init(self, options) {
-            self.css({
-                boxSizing: 'border-box',
-                width: options.width + 'px',
-                height: options.height + 'px',
-            })
-            var canvas = $('<canvas></canvas>')
-            canvas.css({
-                boxSizing: 'border-box',
-                border: options.border,
-            })
-            canvas.attr('width', options.width + 'px')
-            canvas.attr('height', options.height + 'px')
-            self.append(canvas)
-            return canvas
-        }
+  $.fn.sign = function (options) {
+    //需要暴露保存和清除的两个方法
+    if (typeof options == 'string') {
+      return $.fn.sign.methods[options](this);
     }
-    $.fn.sign.default = {
-        width: 600,
-        height: 300,
-        border: 'none',
-        background: '#ccc',
-        lineWidth: 4,
-        lineColor: '#333',
+    var self = $(this)
+    //合并参数
+    options = $.extend({}, $.fn.sign.default, options || {})
+    //canvas具体设置
+    var canDom = init(self, options)[0]
+    var context = canDom.getContext('2d') //画布上下文
+    var canvasX = canDom.getBoundingClientRect().left; // 画板的坐标x
+    var canvasY = canDom.getBoundingClientRect().top; // 画板的坐标y
+    context.fillStyle = options.background; // 画布背景色
+    context.lineWidth = options.lineWidth; // 线的宽度
+    context.strokeStyle = options.lineColor; // 线的颜色
+    context.fillRect(0, 0, canDom.width, canDom.height); // 画板的范围        
+
+    //事件
+    canDom.addEventListener("mousedown", down, false);
+    canDom.addEventListener("mousemove", draw, false);
+    canDom.addEventListener("mouseup", up, false);
+
+    var onoff = false; // 锁定开关
+    var oldx = canvasX; // 起始坐标x
+    var oldy = canvasY; // 起始坐标y
+    var newx; // 结束坐标x
+    var newy; // 结束坐标y
+    // 鼠标按下
+    function down(event) {
+      onoff = true; // 打开开关
+      oldx = event.clientX - canvasX; // 鼠标在画板中点击的X的坐标
+      oldy = event.clientY - canvasY; // 鼠标在画板中点击的Y的坐标
+      context.beginPath(); // 开始路径
     }
-    $.fn.sign.methods = {
-        empty: function (self) {
-            var canDom = self.find('canvas')[0]
-            var context = canDom.getContext('2d')
-            context.clearRect(0, 0, canDom.width, canDom.height)
-            context.fillRect(0, 0, canDom.width, canDom.height)
-        },
-        exportImg: function (self) {
-            //0.5是压缩率，图片是base64，这里选择存到session里面，后续操作待定
-            var expImg = self.find('canvas')[0].toDataURL("image/jpeg", 0.5)
-            sessionStorage.setItem('signImg', expImg)
-            console.log(expImg)
-        }
+
+    // 鼠标移动
+    function draw(event) {
+      // 开关
+      if (onoff) {
+        newx = event.clientX - canvasX;
+        newy = event.clientY - canvasY;
+        context.moveTo(oldx, oldy); // 线的起点坐标
+        context.lineTo(newx, newy); // 线的始点坐标
+        context.stroke(); // 初始化到画布中
+        oldx = newx;
+        oldy = newy;
+      }
     }
+    // 鼠标离开
+    function up() {
+      onoff = false; // 关闭开关
+      context.closePath(); // 关闭路径
+    };
+    //初始化设置容器和画布，并且返回canvas对象
+    function init(self, options) {
+      self.css({
+        boxSizing: 'border-box',
+        width: options.width + 'px',
+        height: options.height + 'px',
+      })
+      var canvas = $('<canvas></canvas>')
+      canvas.css({
+        boxSizing: 'border-box',
+        border: options.border,
+      })
+      canvas.attr('width', options.width + 'px')
+      canvas.attr('height', options.height + 'px')
+      self.append(canvas)
+      return canvas
+    }
+  }
+  $.fn.sign.default = {
+    width: 600,
+    height: 300,
+    border: 'none',
+    background: '#ccc',
+    lineWidth: 4,
+    lineColor: '#333',
+  }
+  $.fn.sign.methods = {
+    empty: function (self) {
+      var canDom = self.find('canvas')[0]
+      var context = canDom.getContext('2d')
+      context.clearRect(0, 0, canDom.width, canDom.height)
+      context.fillRect(0, 0, canDom.width, canDom.height)
+    },
+    exportImg: function (self) {
+      //0.5是压缩率，图片是base64，这里选择存到session里面，后续操作待定
+      var expImg = self.find('canvas')[0].toDataURL("image/jpeg", 0.5)
+      sessionStorage.setItem('signImg', expImg)
+      console.log(expImg)
+    }
+  }
 })($)
--- baseJq/textarea.js
+++ baseJq/textarea.js
@@ -9,65 +9,65 @@
  */
 ;
 (function ($) {
-    //定义扩展方法tarea
-    $.fn.tarea = function (options, param) {
-        if (typeof options === 'string') {
-            return $.fn.tarea.methods[options](this, param);
-        }
-        //参数和默认的参数合并
-        options = $.extend({}, $.fn.tarea['default'], options || {})
-
-        init($(this), options)
-
-        function init(_this, options) {
-            //样式调整，添加textarea和提示框
-            _this.css({
-                width: options.width + 'px',
-                height: options.height + 'px',
-                position: 'relative'
-            })
-
-            //文本输入框
-            var textarea = $('<textarea></textarea>');
-            textarea.css({
-                resize: options.resize,
-                boxSizing: 'border-box',
-                fontSize: options.font + 'px',
-                width: options.width + 'px',
-                height: options.height + 'px',
-                overflow: 'auto'
-            })
-            textarea[0].placeholder = options['text']
-
-            //字数span
-            var span = $('<span><i>' + options.limit + '</i>\/<i>' + options.limit + '</i></span>')
-            span.css({
-                display: 'block',
-                position: 'absolute',
-                bottom: '5px',
-                right: '10px',
-                color: '#999'
-            })
-            _this.append(textarea)
-            _this.append(span)
-            //具体提示
-            var num = _this.find('span i').eq(0)
-            $(textarea).on('keyup', function () {
-                if ($(this).val().length >= options.limit) {
-                    $(this).val($(this).val().slice(0, 200))
-                    alert('输入超过上限')
-                }
-                num.html(options.limit - $(this).val().length)
-            })
-        }
-    };
-    //默认设置
-    $.fn.tarea['default'] = {
-        width: 400,
-        height: 200,
-        font: 14,
-        resize: 'none',
-        limit: 200,
-        text: '大概我就是默认的内容'
+  //定义扩展方法tarea
+  $.fn.tarea = function (options, param) {
+    if (typeof options === 'string') {
+      return $.fn.tarea.methods[options](this, param);
     }
+    //参数和默认的参数合并
+    options = $.extend({}, $.fn.tarea['default'], options || {})
+
+    init($(this), options)
+
+    function init(_this, options) {
+      //样式调整，添加textarea和提示框
+      _this.css({
+        width: options.width + 'px',
+        height: options.height + 'px',
+        position: 'relative'
+      })
+
+      //文本输入框
+      var textarea = $('<textarea></textarea>');
+      textarea.css({
+        resize: options.resize,
+        boxSizing: 'border-box',
+        fontSize: options.font + 'px',
+        width: options.width + 'px',
+        height: options.height + 'px',
+        overflow: 'auto'
+      })
+      textarea[0].placeholder = options['text']
+
+      //字数span
+      var span = $('<span><i>' + options.limit + '</i>\/<i>' + options.limit + '</i></span>')
+      span.css({
+        display: 'block',
+        position: 'absolute',
+        bottom: '5px',
+        right: '10px',
+        color: '#999'
+      })
+      _this.append(textarea)
+      _this.append(span)
+      //具体提示
+      var num = _this.find('span i').eq(0)
+      $(textarea).on('keyup', function () {
+        if ($(this).val().length >= options.limit) {
+          $(this).val($(this).val().slice(0, 200))
+          alert('输入超过上限')
+        }
+        num.html(options.limit - $(this).val().length)
+      })
+    }
+  };
+  //默认设置
+  $.fn.tarea['default'] = {
+    width: 400,
+    height: 200,
+    font: 14,
+    resize: 'none',
+    limit: 200,
+    text: '大概我就是默认的内容'
+  }
 })($)
